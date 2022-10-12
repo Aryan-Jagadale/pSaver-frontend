6b769fefe0a7c0b18b8cb12b24b145d30d93cd4a
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-10-12 20:31:07 +0530

    Solved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import Login from "./pages/Login/Login";
 import Header from "./components/Header/Header";
 import './App.css'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
 
 function App() {
   return (
